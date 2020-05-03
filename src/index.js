79cc96125e235580365acff2714365be6be8e144
Alex Friedman <afriedman1991@gmail.com>
2020-05-03 16:00:39 -0400

    changed some file names

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './index.css';
-import Home from './components/HomeComponent/Home';
+import Home from './pages/HomePage/Home';
 import * as serviceWorker from './serviceWorker';
 
 const App = () => {
